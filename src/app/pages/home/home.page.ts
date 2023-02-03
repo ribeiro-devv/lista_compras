f23156f148b70daaff9e1c44a52e7f6de2865edc
matheusribeiro-dev <matheus.ribeiro2728@gmail.com>
2023-02-02 22:11:55 -0300

    first commit

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,153 @@
+import { Component } from '@angular/core';
+import { ActionSheetController, AlertController, LoadingController } from '@ionic/angular';
+import { readTask } from 'ionicons/dist/types/stencil-public-runtime';
+import { from } from 'rxjs';
+import { TarefaService } from 'src/app/services/tarefa.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  tarefaCollection: any[] = [];
+
+  constructor(
+    private alertCtrl: AlertController,
+    private tarefaService: TarefaService,
+    private actionSheetCtrl: ActionSheetController
+  ) { }
+
+  ionViewDidEnter() {
+    this.listarTarefa();
+  }
+
+  listarTarefa() {
+    this.tarefaCollection = this.tarefaService.listar();
+  }
+
+  async showAdd() {
+    const alert = await this.alertCtrl.create({
+      header: 'Produto',
+      mode: 'ios',
+      inputs: [
+        {
+          name: 'tarefa',
+          type: 'text',
+          placeholder: 'Nome do Produto',
+        },
+        {
+          name: 'quantidade',
+          type: 'number',
+          placeholder: 'Quantidade',
+        }
+
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+          }
+        }, {
+          text: 'Salvar',
+          handler: (tarefa) => {
+            this.tarefaService.salvar(tarefa, () => {
+              this.listarTarefa();
+            });
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+  async delete(item) {
+    const alert = await this.alertCtrl.create({
+      header: 'Excluir Produto?',
+      mode: 'ios',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+          }
+        }, {
+          text: 'Excluir',
+          handler: () => {
+            this.tarefaService.excluir(item, () => {
+              this.listarTarefa();
+            });
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+  // async editar(tarefa) {
+  //   const alert = await this.alertCtrl.create({
+  //     header: 'Tarefa',
+  //     mode: 'ios',
+  //     inputs: [
+  //       {
+  //         name: 'tarefa',
+  //         type: 'text',
+  //         placeholder: tarefa.tarefa
+  //       }
+  //     ],
+  //     buttons: [
+  //       {
+  //         text: 'Cancelar',
+  //         role: 'cancel',
+  //         cssClass: 'secondary',
+  //         handler: () => {
+  //         }
+  //       }, {
+  //         text: 'Salvar',
+  //         handler: (tarefa) => {
+  //           this.tarefaService.edicao(tarefa, () => {
+  //             this.listarTarefa();
+  //           });
+  //         }
+  //       }
+  //     ]
+  //   });
+  //   await alert.present();
+  // }
+  async openActions(tarefa: any) {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'O QUE DESEJA FAZER?',
+      mode: 'ios',
+      buttons: [
+        // {
+        //   text: 'Editar tarefa',
+        //   icon: 'pencil',
+        //   handler: () => {
+        //     this.editar(tarefa);
+        //   },
+        // },
+        {
+          text: tarefa.feito ? 'Colocar como pendente' : 'Marcar como realizado',
+          icon: tarefa.feito ? 'close-circle' : 'checkmark-circle',
+          handler: () => {
+            tarefa.feito = !tarefa.feito
+
+            this.tarefaService.atualizar(tarefa, () => {
+              this.listarTarefa();
+            });
+          },
+        },
+        {
+          text: 'Cancelar',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+          }
+        }
+      ],
+    });
+    await actionSheet.present();
+  }
+}
---
+++ src/app/services/tarefa.service.ts
@@ -0,0 +1,130 @@
+import { Injectable } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TarefaService {
+  tarefaCollection: any[] = [];
+  key: 'tarefaCollection';
+
+  constructor(private actionSheetCtrl: ActionSheetController) { }
+  async salvar(tarefa: any, callback = null) {
+    tarefa.feito = false;
+
+    if(tarefa.tarefa == null || tarefa.tarefa == undefined || tarefa.tarefa == '' ){
+      const actionSheet = this.actionSheetCtrl.create({
+        header: 'O nome do Produto não pode estar vazio',
+        mode: 'ios',
+        buttons: [
+          {
+            text: 'OK',
+            icon: 'close',
+            role: 'cancel',
+            handler: () => {
+            }
+          }
+        ],
+      });
+     (await actionSheet).present();
+    } else{
+      let value = localStorage.getItem(this.key);
+  
+      if (value == null || value == undefined) {
+        this.tarefaCollection.push(tarefa)
+        localStorage.setItem(this.key, JSON.stringify(this.tarefaCollection));
+      }
+      else {
+        let collection: any[] = JSON.parse(value);
+        collection.push(tarefa);
+        localStorage.setItem(this.key, JSON.stringify(collection));
+        
+      }
+  
+      if (callback != null) {
+        callback();
+      }
+
+    }
+
+  }
+
+  listar() {
+    let value = localStorage.getItem(this.key);
+
+    if (value == null || value == undefined) {
+
+      return [];
+    }
+
+    let collection: any[] = JSON.parse(value);
+    return collection;
+  }
+
+  excluir(tarefa: any, callback = null) {
+    let value = localStorage.getItem(this.key);
+
+    if (value == null || value == undefined) {
+
+      return;
+    }
+
+    let collection: any[] = JSON.parse(value);
+
+    let resultCollection = collection.filter(item => {
+      return item.tarefa != tarefa.tarefa
+    });
+
+    localStorage.setItem(this.key, JSON.stringify(resultCollection));
+
+    if (callback != null) {
+      callback();
+    }
+  }
+  atualizar(tarefa: any, callback = null){
+    let value = localStorage.getItem(this.key);
+
+    if (value == null || value == undefined) {
+     return;
+    }
+    else {
+      let collection: any[] = JSON.parse(value);
+
+      collection.forEach(item=>{
+        if(item.tarefa == tarefa.tarefa){
+          item.feito = tarefa.feito
+        }
+      });
+      
+
+      localStorage.setItem(this.key, JSON.stringify(collection));
+    }
+
+    if (callback != null) {
+      callback();
+    }
+  }
+  // edicao(tarefa: any, callback = null){
+
+  //   let value = localStorage.getItem(this.key);
+    
+  //   if (value == null || value == undefined) {
+  //    return;
+  //   }
+  //   else {
+  //     let collection: any[] = JSON.parse(value);
+
+  //     collection.forEach(item=>{
+  //       if(item.tarefa == tarefa.tarefa){
+  //         item.tarefa = tarefa.tarefa;
+  //       }
+  //     });
+      
+  //     localStorage.setItem(this.key, JSON.stringify(collection));
+  //   }
+
+  //   if (callback != null) {
+  //     callback();
+  //   }
+  // }
+}
